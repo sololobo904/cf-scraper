2a7f70ce20803179a4e337838ed2793aee01ed62
sololobo904 <abhishekjha4506@gmail.com>
2020-05-17 00:01:54 +0530

    Yeah.. Just testing out stuffs

---
+++ spec/cf-scraper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CfScraperView from '../lib/cf-scraper-view';
+
+describe('CfScraperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
